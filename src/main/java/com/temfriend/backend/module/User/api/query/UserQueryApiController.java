c6aaff15f0d0728e8acf585824bd53f7097e5dfe
lkdcode <dlrlejr1@naver.com>
2023-12-01 14:57:05 +0900

    Add [UserQueryApiController] : 데이터 조회 관련 RestController

---
+++ src/main/java/com/temfriend/backend/module/User/api/query/UserQueryApiController.java
@@ -0,0 +1,12 @@
+package com.temfriend.backend.module.User.api.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserQueryApiController {
+
+}
