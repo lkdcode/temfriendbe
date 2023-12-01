a9f475acab0529bcec68102f6f6a1435c577b26d
lkdcode <dlrlejr1@naver.com>
2023-12-01 14:54:17 +0900

    Add [AppException] : 전역적으로 사용하게 될 CustomException

---
+++ src/main/java/com/temfriend/backend/global/exception/AppException.java
@@ -0,0 +1,16 @@
+package com.temfriend.backend.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class AppException extends RuntimeException {
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public AppException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.message = exceptionCode.getMessage();
+        this.httpStatus = exceptionCode.getHttpStatus();
+    }
+}
