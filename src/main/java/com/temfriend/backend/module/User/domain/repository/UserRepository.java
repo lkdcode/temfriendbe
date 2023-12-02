222fcc0af24390957eb7d5829eeb3b437a495bdf
lkdcode <dlrlejr1@naver.com>
2023-12-02 23:45:47 +0900

    Delete [User] : delete

--- src/main/java/com/temfriend/backend/module/User/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.temfriend.backend.module.User.domain.repository;
-
-import com.temfriend.backend.module.User.domain.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
