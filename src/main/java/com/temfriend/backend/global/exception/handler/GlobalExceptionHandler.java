fc39293b8509da7e9a68da8044045dd30c3a383f
lkdcode <dlrlejr1@naver.com>
2023-12-02 23:46:14 +0900

    Add [GlobalExceptionHandler] : MethodArgumentNotValidException, RuntimeException handling

--- src/main/java/com/temfriend/backend/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/temfriend/backend/global/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.temfriend.backend.global.exception.custom.AppException;
 import com.temfriend.backend.global.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +19,29 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(ErrorResponse.fail(e));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleRuntimeException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        log.error("Valid. message : {}", message);
+
+        return ResponseEntity
+                .badRequest()
+                .body(message);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
+        String message = e.getMessage();
+        log.error("RuntimeException. message : {}", message);
+
+        return ResponseEntity
+                .internalServerError()
+                .body(message);
     }
 }
