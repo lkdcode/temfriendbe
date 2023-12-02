0e93c03323995215e89f51146a68c43ab6d1d957
lkdcode <dlrlejr1@naver.com>
2023-12-02 23:48:19 +0900

    Feat [Profile] : 유저의 등급 추가, 닉네임은 중복 불가

--- src/main/java/com/temfriend/backend/module/users/domain/Profile.java
+++ src/main/java/com/temfriend/backend/module/users/domain/Profile.java
@@ -1,4 +1,4 @@
-package com.temfriend.backend.module.User.domain;
+package com.temfriend.backend.module.users.domain;
 
 import lombok.*;
 
@@ -9,20 +9,34 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Profile {
+    private enum Grade {
+        BRONZE, SILVER, GOLD
+    }
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nickname;
 
     @Column(nullable = false)
     private String img;
+
+    @Column(nullable = false)
+    private Grade grade;
 
     @Builder
     public Profile(String name, String nickname, String img) {
         this.name = name;
         this.nickname = nickname;
         this.img = img;
+        this.grade = Grade.BRONZE;
+    }
+
+    public void promoteGrade() {
+        switch (this.grade) {
+            case BRONZE -> this.grade = Grade.SILVER;
+            case SILVER -> this.grade = Grade.GOLD;
+        }
     }
 }
