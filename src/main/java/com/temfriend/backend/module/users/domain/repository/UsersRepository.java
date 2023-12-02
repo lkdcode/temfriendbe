f652daedf57501b20399643858df772a701cf5fd
lkdcode <dlrlejr1@naver.com>
2023-12-02 23:49:42 +0900

    Feat [UsersRepository] : UsersRepository

---
+++ src/main/java/com/temfriend/backend/module/users/domain/repository/UsersRepository.java
@@ -0,0 +1,14 @@
+package com.temfriend.backend.module.users.domain.repository;
+
+import com.temfriend.backend.module.users.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
