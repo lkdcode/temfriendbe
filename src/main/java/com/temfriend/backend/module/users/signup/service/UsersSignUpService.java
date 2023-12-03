01b48e06e6de2f2a0c1d8a0a718af0dfb2aee0a6
lkdcode <dlrlejr1@naver.com>
2023-12-03 14:45:11 +0900

    Refactor [UsersSignUpService] : 회원가입 시 비밀번호 암호화

--- src/main/java/com/temfriend/backend/module/users/signup/service/UsersSignUpService.java
+++ src/main/java/com/temfriend/backend/module/users/signup/service/UsersSignUpService.java
@@ -1,6 +1,8 @@
 package com.temfriend.backend.module.users.signup.service;
 
+import com.temfriend.backend.module.users.common.service.UsersPasswordService;
 import com.temfriend.backend.module.users.common.service.UsersValidator;
+import com.temfriend.backend.module.users.domain.Users;
 import com.temfriend.backend.module.users.domain.repository.UsersRepository;
 import com.temfriend.backend.module.users.signup.dto.rseponse.UsersSignUpResponse;
 import com.temfriend.backend.module.users.signup.mapper.UsersSignUpMapper;
@@ -14,14 +16,17 @@ import static com.temfriend.backend.module.users.signup.dto.request.UsersSignUpR
 @Transactional
 @RequiredArgsConstructor
 public class UsersSignUpService {
+    private static final String SUCCESS_MESSAGE = "회원가입에 성공했습니다.";
     private final UsersRepository usersRepository;
     private final UsersValidator usersValidator;
+    private final UsersPasswordService usersPasswordService;
 
-    public UsersSignUpResponse.Create executeSignup(Create request) {
-        usersValidator.validateSignupRequest(request);
-        usersRepository.save(UsersSignUpMapper.INSTANCE
-                .SignUpDTOToUser(request));
+    public UsersSignUpResponse.Create executeSignUp(Create request) {
+        usersValidator.validateSignUpRequest(request);
+        String encodedPassword = usersPasswordService.encode(request.password());
+        Users users = UsersSignUpMapper.INSTANCE.SignUpDTOToUsers(request, encodedPassword);
+        usersRepository.save(users);
 
-        return new UsersSignUpResponse.Create("회원가입에 성공했습니다.");
+        return new UsersSignUpResponse.Create(SUCCESS_MESSAGE);
     }
 }
