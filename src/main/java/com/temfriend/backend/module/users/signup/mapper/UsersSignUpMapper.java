1f1e6c112d440b09f5a2c6d951c674d3410fb769
lkdcode <dlrlejr1@naver.com>
2023-12-03 14:44:24 +0900

    Refactor [UsersSignUpMapper] : 암호화 비밀번호 매핑 추가

--- src/main/java/com/temfriend/backend/module/users/signup/mapper/UsersSignUpMapper.java
+++ src/main/java/com/temfriend/backend/module/users/signup/mapper/UsersSignUpMapper.java
@@ -3,11 +3,13 @@ package com.temfriend.backend.module.users.signup.mapper;
 import com.temfriend.backend.module.users.domain.Users;
 import com.temfriend.backend.module.users.signup.dto.request.UsersSignUpRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UsersSignUpMapper {
     UsersSignUpMapper INSTANCE = Mappers.getMapper(UsersSignUpMapper.class);
 
-    Users SignUpDTOToUser(UsersSignUpRequest.Create request);
+    @Mapping(target = "password", source = "password")
+    Users SignUpDTOToUsers(UsersSignUpRequest.Create request, String password);
 }
