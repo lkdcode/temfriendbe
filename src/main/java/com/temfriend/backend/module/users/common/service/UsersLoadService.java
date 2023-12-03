618eb1bec02d0d9440fd32d807fef4bd65f06274
lkdcode <dlrlejr1@naver.com>
2023-12-03 14:43:07 +0900

    Feat [UsersLoadService] : 레파지토리에서 유저를 불러오는 서비스

---
+++ src/main/java/com/temfriend/backend/module/users/common/service/UsersLoadService.java
@@ -0,0 +1,27 @@
+package com.temfriend.backend.module.users.common.service;
+
+import com.temfriend.backend.module.users.common.exception.custom.LogInFailException;
+import com.temfriend.backend.module.users.common.exception.custom.NotFoundUsersByEmailException;
+import com.temfriend.backend.module.users.common.exception.enums.UsersException;
+import com.temfriend.backend.module.users.domain.Users;
+import com.temfriend.backend.module.users.domain.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsersLoadService {
+    private final UsersRepository usersRepository;
+
+    public Users loadUsersFromEmail(String email) {
+        return usersRepository.findByEmail(email).orElseThrow(() ->
+                new NotFoundUsersByEmailException(UsersException.NOT_FOUNT_USERS_FROM_EMAIL));
+    }
+
+    public Users loadUsersFromLogInEmail(String email) {
+        return usersRepository.findByEmail(email)
+                .orElseThrow(() -> new LogInFailException(UsersException.LOGIN_FAIL_INVALID_CREDENTIALS));
+    }
+}
