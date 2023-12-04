8e828d1c688a2cb9ba0b9ba08399f278338ca9af
lkdcode <dlrlejr1@naver.com>
2023-12-04 21:19:27 +0900

    Refactor [BaseEntity] : 직렬화만 가능하도록 변경, 권한 목록 추가

--- src/main/java/com/temfriend/backend/global/security/CustomUsersDetail.java
+++ src/main/java/com/temfriend/backend/global/security/CustomUsersDetail.java
@@ -1,11 +1,16 @@
 package com.temfriend.backend.global.security;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.*;
 
 @Getter
 @Builder
@@ -14,37 +19,46 @@ import java.util.List;
 public class CustomUsersDetail implements UserDetails {
     private Long id;
     private String email;
+    private List<String> authorities;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public String getPassword() {
         return "";
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public String getUsername() {
-        return "";
+        return this.email;
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonProperty(access = READ_ONLY)
     @Override
     public boolean isEnabled() {
         return true;
