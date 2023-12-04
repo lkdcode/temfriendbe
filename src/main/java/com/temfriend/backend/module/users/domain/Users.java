6b974feb6d08d12dc9f7040ac6dfc3d9eab29e9b
lkdcode <dlrlejr1@naver.com>
2023-12-04 21:24:15 +0900

    Refactor [Users] : 직렬화만 가능하도록 수정

--- src/main/java/com/temfriend/backend/module/users/domain/Users.java
+++ src/main/java/com/temfriend/backend/module/users/domain/Users.java
@@ -1,42 +1,52 @@
 package com.temfriend.backend.module.users.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.temfriend.backend.global.domain.BaseEntity;
-import com.temfriend.backend.module.users.domain.enums.Role;
-import lombok.*;
+import com.temfriend.backend.module.users.domain.enums.Authority;
+import com.temfriend.backend.module.users.domain.enums.Grade;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Entity
 public class Users extends BaseEntity {
 
+    @JsonProperty(access = READ_ONLY)
     @Column(nullable = false, unique = true)
     private String email;
-
+    @JsonProperty(access = READ_ONLY)
     @Column(nullable = false)
     private String password;
-
+    @JsonProperty(access = READ_ONLY)
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Grade grade;
+
+    @JsonProperty(access = READ_ONLY)
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
 
     @Embedded
     private Profile profile;
 
     @Builder
-    public Users(String email, String password, String role, String name, String nickname, String img) {
+    public Users(String email, String password, String grade, String name, String nickname, String img) {
         this.email = email;
         this.password = password;
-        this.role = Role.from(role);
+        this.grade = Grade.From(grade);
+        this.authority = Authority.USERS;
         this.profile = Profile.builder()
                 .name(name)
                 .nickname(nickname)
                 .img(img)
                 .build();
-    }
-
-    public void promoteGrade() {
-        this.profile.promoteGrade();
     }
 }
