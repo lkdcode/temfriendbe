93dd949bf05c1054190914b2c51b3080f6388513
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:03:32 +0900

    Refactor [PostsCommandResponseDTO] : 디비 조작 관련 DTO로 알 수 있게 네이밍 변경

--- src/main/java/com/temfriend/backend/module/posts/command/dto/response/PostsCommandResponseDTO.java
+++ src/main/java/com/temfriend/backend/module/posts/command/dto/response/PostsCommandResponseDTO.java
@@ -2,27 +2,27 @@ package com.temfriend.backend.module.posts.command.dto.response;
 
 import lombok.Builder;
 
-import static com.temfriend.backend.module.posts.command.dto.response.PostsResponseDTO.*;
+import static com.temfriend.backend.module.posts.command.dto.response.PostsCommandResponseDTO.*;
 
-public sealed interface PostsResponseDTO permits Create, Update, Delete {
+public sealed interface PostsCommandResponseDTO permits Create, Update, Delete {
     @Builder
     record Create(
             Long id,
             String message
-    ) implements PostsResponseDTO {
+    ) implements PostsCommandResponseDTO {
     }
 
     @Builder
     record Update(
             Long id,
             String message
-    ) implements PostsResponseDTO {
+    ) implements PostsCommandResponseDTO {
     }
 
     @Builder
     record Delete(
             Long id,
             String message
-    ) implements PostsResponseDTO {
+    ) implements PostsCommandResponseDTO {
     }
 }
