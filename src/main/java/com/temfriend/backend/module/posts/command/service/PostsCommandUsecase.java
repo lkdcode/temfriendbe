4467359a55e0fc40ac6c044fa3a8be85f46d2472
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:05:04 +0900

    Refactor [PostsCommandUsecase] : 게시글 수정 오버로딩 메서드 삭제

--- src/main/java/com/temfriend/backend/module/posts/command/service/PostsCommandUsecase.java
+++ src/main/java/com/temfriend/backend/module/posts/command/service/PostsCommandUsecase.java
@@ -1,18 +1,14 @@
 package com.temfriend.backend.module.posts.command.service;
 
 import com.temfriend.backend.global.security.CustomUsersDetail;
-import com.temfriend.backend.module.posts.command.dto.request.PostsRequestDTO;
-import com.temfriend.backend.module.posts.command.dto.response.PostsResponseDTO;
+import com.temfriend.backend.module.posts.command.dto.request.PostsCommandRequestDTO;
+import com.temfriend.backend.module.posts.command.dto.response.PostsCommandResponseDTO;
 
 public interface PostsCommandUsecase {
-    PostsResponseDTO.Create executePostsSave(PostsRequestDTO.Create request, CustomUsersDetail customUsersDetail);
+    PostsCommandResponseDTO.Create executePostsSave(PostsCommandRequestDTO.Create request, CustomUsersDetail customUsersDetail);
 
-    PostsResponseDTO.Update executePostsUpdate(PostsRequestDTO.Update request, CustomUsersDetail customUsersDetail, Long postsId);
+    PostsCommandResponseDTO.Update executePostsUpdate(PostsCommandRequestDTO.Update request, CustomUsersDetail customUsersDetail, Long postsId);
 
-    PostsResponseDTO.Update executePostsUpdate(PostsRequestDTO.UpdateOnlyTitle request, CustomUsersDetail customUsersDetail, Long postsId);
-
-    PostsResponseDTO.Update executePostsUpdate(PostsRequestDTO.UpdateOnlyContent request, CustomUsersDetail customUsersDetail, Long postsId);
-
-    PostsResponseDTO.Delete executePostsDelete(Long id, CustomUsersDetail customUsersDetail);
+    PostsCommandResponseDTO.Delete executePostsDelete(Long id, CustomUsersDetail customUsersDetail);
 
 }
