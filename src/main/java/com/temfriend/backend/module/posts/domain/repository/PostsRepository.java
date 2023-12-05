19a6e7b77cd98b28bb903bd4b10b4feff15ebbcd
lkdcode <dlrlejr1@naver.com>
2023-12-05 12:51:41 +0900

    Feat [PostsRepository] : 게시글 레파지토리

---
+++ src/main/java/com/temfriend/backend/module/posts/domain/repository/PostsRepository.java
@@ -0,0 +1,9 @@
+package com.temfriend.backend.module.posts.domain.repository;
+
+import com.temfriend.backend.module.posts.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
