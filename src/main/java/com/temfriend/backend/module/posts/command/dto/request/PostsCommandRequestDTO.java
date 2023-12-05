39c97ea4982aa395988d3c8521660736ab7ce317
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:03:17 +0900

    Refactor [PostsCommandRequestDTO] : 디비 조작 관련 DTO로 알 수 있게 네이밍 변경

--- src/main/java/com/temfriend/backend/module/posts/command/dto/request/PostsCommandRequestDTO.java
+++ src/main/java/com/temfriend/backend/module/posts/command/dto/request/PostsCommandRequestDTO.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import static com.temfriend.backend.module.posts.command.dto.request.PostsRequestDTO.*;
+import static com.temfriend.backend.module.posts.command.dto.request.PostsCommandRequestDTO.*;
 
-public sealed interface PostsRequestDTO permits Create, Update, UpdateOnlyTitle, UpdateOnlyContent {
+public sealed interface PostsCommandRequestDTO permits Create, Update {
     @Builder
     record Create(
             @NotBlank(message = "제목을 입력해주세요.")
@@ -16,7 +16,7 @@ public sealed interface PostsRequestDTO permits Create, Update, UpdateOnlyTitle,
             @NotBlank(message = "내용을 입력해주세요.")
             @Size(min = 5, max = 2_000, message = "내용은 최소 5글자 최대 2,000글자입니다.")
             String content
-    ) implements PostsRequestDTO {
+    ) implements PostsCommandRequestDTO {
     }
 
     @Builder
@@ -27,22 +27,6 @@ public sealed interface PostsRequestDTO permits Create, Update, UpdateOnlyTitle,
             @NotBlank(message = "수정할 내용을 입력해주세요.")
             @Size(min = 5, max = 2_000, message = "수정할 내용은 최소 5글자 최대 2,000글자입니다.")
             String content
-    ) implements PostsRequestDTO {
-    }
-
-    @Builder
-    record UpdateOnlyTitle(
-            @NotBlank(message = "수정할 제목을 입력해주세요.")
-            @Size(min = 2, max = 50, message = "수정할 제목은 최소 2글자 최대 50글자입니다.")
-            String title
-    ) implements PostsRequestDTO {
-    }
-
-    @Builder
-    record UpdateOnlyContent(
-            @NotBlank(message = "수정할 내용을 입력해주세요.")
-            @Size(min = 5, max = 2_000, message = "수정할 내용은 최소 5글자 최대 2,000글자입니다.")
-            String content
-    ) implements PostsRequestDTO {
+    ) implements PostsCommandRequestDTO {
     }
 }
