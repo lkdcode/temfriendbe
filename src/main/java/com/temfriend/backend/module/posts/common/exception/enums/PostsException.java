936d5814135cd5c59fa907e3d6f57c2c74a2e7b5
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:05:37 +0900

    Add [PostsException] : 게시글 작성자 미스 매치 에러 코드 추가

--- src/main/java/com/temfriend/backend/module/posts/common/exception/enums/PostsException.java
+++ src/main/java/com/temfriend/backend/module/posts/common/exception/enums/PostsException.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PostsException implements ExceptionCode {
     NOT_FOUNT_POSTS_FROM_ID(HttpStatus.NOT_FOUND, "해당 게시글은 존재하지 않습니다."),
+    NOT_MATCHES(HttpStatus.UNAUTHORIZED, "해당 게시글의 작성자가 아닙니다."),
     ;
     private final HttpStatus httpStatus;
     private final String message;
