b5747c42e1ea9dbec6b66c00c0eb80dfc6517a0a
lkdcode <dlrlejr1@naver.com>
2023-12-05 12:51:00 +0900

    Feat [PostsException] : 게시글 관련 에러 코드

---
+++ src/main/java/com/temfriend/backend/module/posts/common/exception/enums/PostsException.java
@@ -0,0 +1,16 @@
+package com.temfriend.backend.module.posts.common.exception.enums;
+
+import com.temfriend.backend.global.exception.custom.ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PostsException implements ExceptionCode {
+    NOT_FOUNT_POSTS_FROM_ID(HttpStatus.NOT_FOUND, "해당 게시글은 존재하지 않습니다."),
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
