02041dc3d36fa29727ccea14eadd9b7caab4eea4
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:10:17 +0900

    Refactor [PostsQueryMapper] : ResponseDTO 네이밍 수정

--- src/main/java/com/temfriend/backend/module/posts/query/mapper/response/PostsQueryMapper.java
+++ src/main/java/com/temfriend/backend/module/posts/query/mapper/response/PostsQueryMapper.java
@@ -1,7 +1,7 @@
 package com.temfriend.backend.module.posts.query.mapper.response;
 
 import com.temfriend.backend.module.posts.domain.Posts;
-import com.temfriend.backend.module.posts.query.dto.response.PostsQueryResponse;
+import com.temfriend.backend.module.posts.query.dto.response.PostsQueryResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -12,7 +12,7 @@ public interface PostsQueryMapper {
 
     PostsQueryMapper INSTANCE = Mappers.getMapper(PostsQueryMapper.class);
 
-    PostsQueryResponse.Get createResponseGetFrom(Posts posts);
+    PostsQueryResponseDTO.Get createResponseGetFrom(Posts posts);
 
-    List<PostsQueryResponse.Get> createListResponseGetFrom(List<Posts> postsList);
+    List<PostsQueryResponseDTO.Get> createListResponseGetFrom(List<Posts> postsList);
 }
