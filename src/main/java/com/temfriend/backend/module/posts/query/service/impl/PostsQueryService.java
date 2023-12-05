a35fa11c18fefc5e6c4b3c7e5ddc67dd3b34bfa7
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:08:46 +0900

    Feat [PostsQueryService] : Posts 조회 관련 Service

---
+++ src/main/java/com/temfriend/backend/module/posts/query/service/impl/PostsQueryService.java
@@ -0,0 +1,47 @@
+package com.temfriend.backend.module.posts.query.service.impl;
+
+import com.temfriend.backend.module.posts.common.service.PostsLoadService;
+import com.temfriend.backend.module.posts.domain.Posts;
+import com.temfriend.backend.module.posts.query.dto.response.PostsQueryResponseDTO;
+import com.temfriend.backend.module.posts.query.mapper.response.PostsQueryMapper;
+import com.temfriend.backend.module.posts.query.service.PostsQueryUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PostsQueryService implements PostsQueryUsecase {
+    private final PostsLoadService postsLoadService;
+
+    @Override
+    public PostsQueryResponseDTO.Get retrieveSinglePostsById(Long id) {
+        Posts posts = postsLoadService.loadPostsFromId(id);
+
+        return PostsQueryMapper.INSTANCE
+                .createResponseGetFrom(posts);
+    }
+
+    @Override
+    public List<PostsQueryResponseDTO.Get> retrievePostsList(Pageable pageable) {
+        List<Posts> postsList = postsLoadService.loadPostsList(pageable);
+
+        return getListResponseGetFrom(postsList);
+    }
+
+    @Override
+    public List<PostsQueryResponseDTO.Get> retrievePostsListByUsersId(Long usersId) {
+        List<Posts> postsList = postsLoadService.loadPostsListByUsersId(usersId);
+
+        return getListResponseGetFrom(postsList);
+    }
+
+    private List<PostsQueryResponseDTO.Get> getListResponseGetFrom(List<Posts> postsList) {
+        return PostsQueryMapper.INSTANCE
+                .createListResponseGetFrom(postsList);
+    }
+}
