aade4ccf23a7722c65687894b6e868549470a3b4
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:02:18 +0900

    Refactor [PostsCommandApiController] : 컨트롤러 매개변수 순서 변경

--- src/main/java/com/temfriend/backend/module/posts/command/api/PostsCommandApiController.java
+++ src/main/java/com/temfriend/backend/module/posts/command/api/PostsCommandApiController.java
@@ -2,14 +2,15 @@ package com.temfriend.backend.module.posts.command.api;
 
 import com.temfriend.backend.global.response.SuccessResponse;
 import com.temfriend.backend.global.security.CustomUsersDetail;
-import com.temfriend.backend.module.posts.command.dto.request.PostsRequestDTO;
-import com.temfriend.backend.module.posts.command.dto.response.PostsResponseDTO;
+import com.temfriend.backend.module.posts.command.dto.request.PostsCommandRequestDTO;
+import com.temfriend.backend.module.posts.command.dto.response.PostsCommandResponseDTO;
 import com.temfriend.backend.module.posts.command.service.PostsCommandUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,50 +19,30 @@ public class PostsCommandApiController {
     private final PostsCommandUsecase postsCommandUsecase;
 
     @PostMapping
-    public SuccessResponse<PostsResponseDTO.Create> create(
-            @AuthenticationPrincipal CustomUsersDetail customUsersDetail
-            , @RequestBody @Valid PostsRequestDTO.Create request
+    public SuccessResponse<PostsCommandResponseDTO.Create> getCreate(
+            @RequestBody @Valid PostsCommandRequestDTO.Create request
+            , @AuthenticationPrincipal CustomUsersDetail customUsersDetail
     ) {
-        PostsResponseDTO.Create response = postsCommandUsecase.executePostsSave(request, customUsersDetail);
+        PostsCommandResponseDTO.Create response = postsCommandUsecase.executePostsSave(request, customUsersDetail);
         return SuccessResponse.ok(response);
     }
 
     @PutMapping("/{id}")
-    public SuccessResponse<PostsResponseDTO.Update> update(
-            @AuthenticationPrincipal CustomUsersDetail customUsersDetail
-            , @PathVariable(name = "id") Long id
-            , @RequestBody @Valid PostsRequestDTO.Update request
+    public SuccessResponse<PostsCommandResponseDTO.Update> getUpdate(
+            @RequestBody @Valid PostsCommandRequestDTO.Update request
+            , @AuthenticationPrincipal CustomUsersDetail customUsersDetail
+            , @PathVariable(name = "id") @Positive Long id
     ) {
-        PostsResponseDTO.Update response = postsCommandUsecase.executePostsUpdate(request, customUsersDetail, id);
-        return SuccessResponse.ok(response);
-    }
-
-    @PutMapping("/{id}")
-    public SuccessResponse<PostsResponseDTO.Update> update(
-            @AuthenticationPrincipal CustomUsersDetail customUsersDetail
-            , @PathVariable(name = "id") Long id
-            , @RequestBody @Valid PostsRequestDTO.UpdateOnlyTitle request
-    ) {
-        PostsResponseDTO.Update response = postsCommandUsecase.executePostsUpdate(request, customUsersDetail, id);
-        return SuccessResponse.ok(response);
-    }
-
-    @PutMapping("/{id}")
-    public SuccessResponse<PostsResponseDTO.Update> update(
-            @AuthenticationPrincipal CustomUsersDetail customUsersDetail
-            , @PathVariable(name = "id") Long id
-            , @RequestBody @Valid PostsRequestDTO.UpdateOnlyContent request
-    ) {
-        PostsResponseDTO.Update response = postsCommandUsecase.executePostsUpdate(request, customUsersDetail, id);
+        PostsCommandResponseDTO.Update response = postsCommandUsecase.executePostsUpdate(request, customUsersDetail, id);
         return SuccessResponse.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public SuccessResponse<?> delete(
-            @AuthenticationPrincipal CustomUsersDetail customUsersDetail
-            , @PathVariable(name = "id") Long id
+    public SuccessResponse<PostsCommandResponseDTO.Delete> getDelete(
+            @PathVariable(name = "id") @Positive Long id
+            , @AuthenticationPrincipal CustomUsersDetail customUsersDetail
     ) {
-
-        return SuccessResponse.ok(null);
+        PostsCommandResponseDTO.Delete response = postsCommandUsecase.executePostsDelete(id, customUsersDetail);
+        return SuccessResponse.ok(response);
     }
 }
