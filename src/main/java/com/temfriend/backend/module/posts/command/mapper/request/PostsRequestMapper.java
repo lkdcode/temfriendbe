48ea0a1c7ab75af734d4e8a0dbbe023b35aa6209
lkdcode <dlrlejr1@naver.com>
2023-12-06 02:09:42 +0900

    Feat [PostsRequestMapper] : Posts 관련 Request 를 Entity 로 변환

--- src/main/java/com/temfriend/backend/module/posts/command/mapper/request/PostsRequestMapper.java
+++ src/main/java/com/temfriend/backend/module/posts/command/mapper/request/PostsRequestMapper.java
@@ -1,6 +1,6 @@
 package com.temfriend.backend.module.posts.command.mapper.request;
 
-import com.temfriend.backend.module.posts.command.dto.request.PostsRequestDTO;
+import com.temfriend.backend.module.posts.command.dto.request.PostsCommandRequestDTO;
 import com.temfriend.backend.module.posts.domain.Posts;
 import com.temfriend.backend.module.users.domain.Users;
 import org.mapstruct.Mapper;
@@ -10,6 +10,6 @@ import org.mapstruct.factory.Mappers;
 public interface PostsRequestMapper {
     PostsRequestMapper INSTANCE = Mappers.getMapper(PostsRequestMapper.class);
 
-    Posts createDTOToPosts(PostsRequestDTO.Create request, Users users);
+    Posts createDTOToPosts(PostsCommandRequestDTO.Create request, Users users);
 
 }
