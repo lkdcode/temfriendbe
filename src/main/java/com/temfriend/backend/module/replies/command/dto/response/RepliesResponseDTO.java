db485995128ffb02c434f30ce510bca86ccb75b8
lkdcode <dlrlejr1@naver.com>
2023-12-08 12:57:45 +0900

    Feat [RepliesResponseDTO] : RepliesResponseDTO

---
+++ src/main/java/com/temfriend/backend/module/replies/command/dto/response/RepliesResponseDTO.java
@@ -0,0 +1,29 @@
+package com.temfriend.backend.module.replies.command.dto.response;
+
+import lombok.Builder;
+
+import static com.temfriend.backend.module.replies.command.dto.response.RepliesResponseDTO.*;
+
+public sealed interface RepliesResponseDTO permits Create, Update, Delete {
+    @Builder
+    record Create(
+            Long id,
+            String content,
+            String message
+    ) implements RepliesResponseDTO {
+    }
+
+    @Builder
+    record Update(
+            Long id,
+            String content,
+            String message
+    ) implements RepliesResponseDTO {
+    }
+
+    @Builder
+    record Delete(
+            String message
+    ) implements RepliesResponseDTO {
+    }
+}
