b7e479ec1318e17db50f37467cc50e548a12c41a
lkdcode <dlrlejr1@naver.com>
2023-12-09 23:39:05 +0900

    Add [RepliesRepository] : 게시글 아이디로 댓글 리스트 조회 JPA 메서드 쿼리

--- src/main/java/com/temfriend/backend/module/replies/domain/repository/RepliesRepository.java
+++ src/main/java/com/temfriend/backend/module/replies/domain/repository/RepliesRepository.java
@@ -5,9 +5,12 @@ import com.temfriend.backend.module.users.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RepliesRepository extends JpaRepository<Replies, Long> {
     Optional<Replies> findByUsers(Users users);
+
+    List<Replies> findByPostsId(Long postsId);
 }
