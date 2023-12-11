9b0da815649a75eb15232a48cfd8b86cee35bb0c
lkdcode <dlrlejr1@naver.com>
2023-12-11 17:08:15 +0900

    Refactor [ActivitiesCommandService] : 매개변수 Users 로 변경

--- src/main/java/com/temfriend/backend/module/activities/service/command/impl/ActivitiesCommandService.java
+++ src/main/java/com/temfriend/backend/module/activities/service/command/impl/ActivitiesCommandService.java
@@ -1,10 +1,8 @@
 package com.temfriend.backend.module.activities.service.command.impl;
 
-import com.temfriend.backend.global.security.CustomUsersDetail;
 import com.temfriend.backend.module.activities.domain.Activities;
 import com.temfriend.backend.module.activities.service.ActivitiesLoadService;
 import com.temfriend.backend.module.activities.service.command.ActivitiesCommandUsecase;
-import com.temfriend.backend.module.users.common.service.UsersLoadService;
 import com.temfriend.backend.module.users.domain.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,22 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ActivitiesCommandService implements ActivitiesCommandUsecase {
     private final ActivitiesLoadService activitiesLoadService;
-    private final UsersLoadService usersLoadService;
 
     @Override
-    public void executeUpdateLoginTime(CustomUsersDetail customUsersDetail) {
-        Activities histories = findActivitiesBy(customUsersDetail);
+    public void executeUpdateLoginTime(Users users) {
+        Activities histories = activitiesLoadService.loadActivitiesByUsers(users);
         histories.recordLoginTime();
     }
 
     @Override
-    public void executeUpdatePostsCreateTime(CustomUsersDetail customUsersDetail) {
-        Activities histories = findActivitiesBy(customUsersDetail);
+    public void executeUpdatePostsCreateTime(Users users) {
+        Activities histories = activitiesLoadService.loadActivitiesByUsers(users);
         histories.recordPostsCreationTime();
-    }
-
-    private Activities findActivitiesBy(CustomUsersDetail customUsersDetail) {
-        Users users = usersLoadService.loadUsersFromEmail(customUsersDetail.getEmail());
-        return activitiesLoadService.loadActivitiesByUsers(users);
     }
 }
