d9c41a2be1f00539d4f5a97e30c89066a5d8da46
lkdcode <dlrlejr1@naver.com>
2023-12-11 17:14:01 +0900

    Refactor [UsersSignUpUsecase] : 회원가입 관련 usecase interface

--- src/main/java/com/temfriend/backend/module/users/signup/api/UsersSignUpApiController.java
+++ src/main/java/com/temfriend/backend/module/users/signup/api/UsersSignUpApiController.java
@@ -3,7 +3,7 @@ package com.temfriend.backend.module.users.signup.api;
 import com.temfriend.backend.global.common.response.SuccessResponse;
 import com.temfriend.backend.module.users.signup.dto.request.UsersSignUpRequest;
 import com.temfriend.backend.module.users.signup.dto.rseponse.UsersSignUpResponse;
-import com.temfriend.backend.module.users.signup.service.UsersSignUpService;
+import com.temfriend.backend.module.users.signup.service.UsersSignUpUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +16,14 @@ import javax.validation.Valid;
 @RequestMapping("${api.users}")
 @RequiredArgsConstructor
 public class UsersSignUpApiController {
-    private final UsersSignUpService usersSignUpService;
+    private final UsersSignUpUsecase usersSignUpUsecase;
 
     @PostMapping("/sign-up")
     public SuccessResponse<UsersSignUpResponse.Create> getSignUp(
             @RequestBody @Valid UsersSignUpRequest.Create request
     ) {
-        UsersSignUpResponse.Create response = usersSignUpService.executeSignUp(request);
+        UsersSignUpResponse.Create response = usersSignUpUsecase.executeSignUp(request);
+
         return SuccessResponse
                 .ok(response);
     }
---
+++ src/main/java/com/temfriend/backend/module/users/signup/service/impl/UsersSignUpService.java
@@ -0,0 +1,46 @@
+package com.temfriend.backend.module.users.signup.service.impl;
+
+import com.temfriend.backend.module.points.command.PointsCommandUsecase;
+import com.temfriend.backend.module.users.common.service.UsersPasswordService;
+import com.temfriend.backend.module.users.common.service.UsersValidator;
+import com.temfriend.backend.module.users.domain.Users;
+import com.temfriend.backend.module.users.domain.repository.UsersRepository;
+import com.temfriend.backend.module.users.signup.dto.request.UsersSignUpRequest;
+import com.temfriend.backend.module.users.signup.dto.rseponse.UsersSignUpResponse;
+import com.temfriend.backend.module.users.signup.mapper.UsersSignUpMapper;
+import com.temfriend.backend.module.users.signup.service.UsersSignUpUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsersSignUpService implements UsersSignUpUsecase {
+    private static final String SIGN_UP_SUCCESS_MESSAGE = "회원가입에 성공했습니다.";
+    private final UsersRepository usersRepository;
+    private final UsersValidator usersValidator;
+    private final UsersPasswordService usersPasswordService;
+    private final PointsCommandUsecase pointsCommandUsecase;
+
+    @Override
+    public UsersSignUpResponse.Create executeSignUp(UsersSignUpRequest.Create request) {
+        Users users = makeUsers(request);
+        Users savedUsers = usersRepository.save(users);
+        pointsCommandUsecase.executeCreatePointsByUsers(savedUsers);
+
+        return UsersSignUpResponse.Create.builder()
+                .message(SIGN_UP_SUCCESS_MESSAGE)
+                .build();
+    }
+
+    private Users makeUsers(UsersSignUpRequest.Create request) {
+        usersValidator.validateSignUpRequest(request);
+        String encodedPassword = makeEncodedPassword(request);
+        return UsersSignUpMapper.INSTANCE.convertUsersFrom(request, encodedPassword);
+    }
+
+    private String makeEncodedPassword(UsersSignUpRequest.Create request) {
+        return usersPasswordService.encode(request.password());
+    }
+}
