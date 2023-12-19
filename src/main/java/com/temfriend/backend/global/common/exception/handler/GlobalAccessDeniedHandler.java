9c8633cfe09a01152de9abb9be03acd6f6fec1e9
lkdcode <dlrlejr1@naver.com>
2023-12-19 18:55:29 +0900

    Feat [GlobalAccessDeniedHandler] : 인가 예외 핸들링

---
+++ src/main/java/com/temfriend/backend/global/common/exception/handler/GlobalAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.temfriend.backend.global.common.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class GlobalAccessDeniedHandler implements AccessDeniedHandler {
+    private static final String MESSAGE = "접근 권한이 없습니다.";
+    private static final String UTF = "UTF-8";
+    private static final String CHAR_SET = ";charset=UTF-8";
+
+    @Override
+    public void handle(
+            HttpServletRequest request
+            , HttpServletResponse response
+            , AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("[AccessDeniedHandler] : 접근 권한이 없습니다. responseMessage : {}", MESSAGE);
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE + CHAR_SET);
+        response.setCharacterEncoding(UTF);
+        response.getWriter().write(MESSAGE);
+    }
+}
